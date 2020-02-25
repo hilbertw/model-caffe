e5a7c6150d8f5b0c9ccda687bd33f74d7f330c57
Your Name <you@example.com>
2020-02-26 06:47:14 +0800

    add ref

---
+++ ref/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+threshold="0.2"
+
+
+
+if [ $# -gt 0 ];
+then
+threshold="$1"
+fi
+
+
+#while((1==1));
+#do
+build/ssd_detect \
+./models/VGGNet/VOC0712Plus/SSD_512x512/deploy.prototxt \
+./models/VGGNet/VOC0712Plus/SSD_512x512/VGG_VOC0712Plus_SSD_512x512_iter_240000.caffemodel \
+--img_file aaa-tests-pedestrian-detection-systems-in-2019-models_100718929_l.jpg \
+--confidence_threshold ${threshold}  
+
+#done;
+
