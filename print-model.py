e714c482f7474abd275655a1eac0225f10ae66d7
Your Name <you@example.com>
2020-03-26 16:00:15 +0800

    add blob name

--- print-model.py
+++ print-model.py
@@ -43,27 +43,33 @@ m=[]
 i=0
 for l in net.layers:
     n=[]
+    n1=[]
     for id in net._top_ids(i):
         j=find_in_bottom(net,id)
         if j>=0:
             n.append(net._layer_names[j])
-    nn=[]
+        n1.append(net._blob_names[id])
+    n2=[]
+    n3=[]
     for id in net._bottom_ids(i):
         j=find_in_top(net,id)
         if j>=0:
-            nn.append(net._layer_names[j])
+            n2.append(net._layer_names[j])
+        n3.append(net._blob_names[id])
 
-    m.append((','.join(n),','.join(nn)))
+    m.append((','.join(n),','.join(n2),','.join(n1),','.join(n3)))
     i=i+1
 
 i=0
 
 with open("model.txt","w") as f:
-   for n,nn in m:
+   for n,n2,n1,n3 in m:
         name=net._layer_names[i]
         f.write('\n%s:\n'%(name))
-        f.write('[%s]\n'%(nn))  
-        f.write('[%s]\n'%(n))  
+        f.write('bottom:[%s]\n'%(n2))  
+        f.write('top:[%s]\n'%(n))  
+        f.write('bottom:[%s]\n'%(n3))  
+        f.write('top:[%s]\n'%(n1))  
         i=i+1
    f.close()
 
