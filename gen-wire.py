89a6ea96244dfa1d104ed680f5953a7b10099bae
Your Name <you@example.com>
2020-03-27 16:33:37 +0800

    e109

--- gen-wire.py
+++ gen-wire.py
@@ -5,22 +5,22 @@ import argparse
 import os
 import sys
 
-def find_in_bottom(net,id):
-    i=0
-    for l in net.layers:
+def find_in_bottom(net,id,start):
+    i=start+1
+    while i<len( net.layers):
         ids=net._bottom_ids(i)
         if id in ids:
            return i
         i=i+1
     return -1
 
-def find_in_top(net,id):
-    i=0
-    for l in net.layers:
+def find_in_top(net,id,start):
+    i=start-1
+    while i>=0:
         ids=net._top_ids(i)
         if id in ids:
            return i
-        i=i+1
+        i=i-1
     return -1
 
 def print_top_wire(f,net,i):
@@ -31,23 +31,23 @@ def print_top_wire(f,net,i):
         f.write("%s.top_blob_empty.write(true);\n"%(self))
         return 
      if n==1:
-       l=find_in_bottom(net,top_ids[0])
+       l=find_in_bottom(net,top_ids[0],i)
        if l>=0:
            bottom= net._layer_names[l]
            f.write("%s.top_blob_empty(%s.bottom_blob_empty);\n"%(self,bottom))
        else:
            f.write("%s.top_blob_empty(output_empty);\n"%(self))
      else:
-       i=0
+       j=0
        for id in top_ids:
-           l=find_in_bottom(net,id)
+           l=find_in_bottom(net,id,i)
            if l>=0:
              bottom=net._layer_names[l]
-             f.write("%s_top_and.in[%d](%s.bottom_blob_empty);\n"%(self,i,bottom))
+             f.write("%s_top_and.in[%d](%s.bottom_blob_empty);\n"%(self,j,bottom))
            else:
-             f.write("%s_top_and.in[%d](output_empty);\n"%(self,i))
+             f.write("%s_top_and.in[%d](output_empty);\n"%(self,j))
               
-           i=i+1
+           j=j+1
 
        f.write("%s.top_blob_empty(%s_top_and.out);\n"%(self,self))
 
@@ -59,22 +59,22 @@ def print_bottom_wire(f,net,i):
         f.write("%s.bottom_blob_filled(input_filled);\n"%(self))
         return
      if n==1:
-       l=find_in_top(net,bottom_ids[0])
+       l=find_in_top(net,bottom_ids[0],i)
        if l>=0:
-          top= net._layer_names[bottom_ids[0]]
+          top= net._layer_names[l]
           f.write("%s.bottom_blob_filled(%s.top_blob_filled);\n"%(self,top))
        else:
           f.write("%s.bottom_blob_filled(input_filled);\n"%(self))
      else:
-       i=0
+       j=0
        for id in bottom_ids:
-           l=find_in_top(net,id)
+           l=find_in_top(net,id,i)
            if l>=0:
               top=net._layer_names[l]
-              f.write("%s_bottom_and.in[%d](%s.top_blob_filled);\n"%(self,i,top))
+              f.write("%s_bottom_and.in[%d](%s.top_blob_filled);\n"%(self,j,top))
            else:
-              f.write("%s_bottom_and.in[%d](input_filled);\n"%(self,i,top))
-           i=i+1
+              f.write("%s_bottom_and.in[%d](input_filled);\n"%(self,j))
+           j=j+1
 
        f.write("%s.bottom_blob_filled(%s_bottom_and.out);\n"%(self,self))
 
@@ -114,7 +114,7 @@ void sc_net::setup_wires()
 
      s=[]
      for id in net._inputs:
-         l=find_in_bottom(net,id)
+         l=find_in_bottom(net,id,0)
          if l<0:
              raise(0)
          if not l in s:
@@ -136,7 +136,7 @@ void sc_net::setup_wires()
           
      s=[]
      for id in net._outputs:
-         l=find_in_top(net,id)
+         l=find_in_top(net,id,len(net._layer_names))
          if l<0:
             raise(0)
          if not l in s:
--- print-model.py
+++ print-model.py
@@ -6,22 +6,22 @@ import os
 import sys
 
 
-def find_in_bottom(net,id):
-    i=0
-    for l in net.layers:
+def find_in_bottom(net,id,start):
+    i=start+1
+    while i<len( net.layers):
         ids=net._bottom_ids(i)
         if id in ids:
            return i
         i=i+1
     return -1
 
-def find_in_top(net,id):
-    i=0
-    for l in net.layers:
+def find_in_top(net,id,start):
+    i=start-1
+    while i>=0:
         ids=net._top_ids(i)
         if id in ids:
            return i
-        i=i+1
+        i=i-1
     return -1
 
 
@@ -45,14 +45,14 @@ for l in net.layers:
     n=[]
     n1=[]
     for id in net._top_ids(i):
-        j=find_in_bottom(net,id)
+        j=find_in_bottom(net,id,i)
         if j>=0:
             n.append(net._layer_names[j])
         n1.append(net._blob_names[id])
     n2=[]
     n3=[]
     for id in net._bottom_ids(i):
-        j=find_in_top(net,id)
+        j=find_in_top(net,id,i)
         if j>=0:
             n2.append(net._layer_names[j])
         n3.append(net._blob_names[id])
@@ -68,8 +68,8 @@ with open("model.txt","w") as f:
         f.write('\n%s:\n'%(name))
         f.write('bottom:[%s]\n'%(n2))  
         f.write('top:[%s]\n'%(n))  
-        f.write('bottom:[%s]\n'%(n3))  
-        f.write('top:[%s]\n'%(n1))  
+        f.write('bottom blob:[%s]\n'%(n3))  
+        f.write('top blob:[%s]\n'%(n1))  
         i=i+1
    f.close()
 
