610b8452f531e2924ebbeb2c9c40a58fa9c2488a
Your Name <you@example.com>
2020-04-04 05:30:00 +0800

    add net wrapper

--- gen-blob-cpp.py
+++ gen-blob-cpp.py
@@ -4,87 +4,9 @@ import numpy as np
 import argparse
 import os
 import sys
-
+from net_wrapper import  net_wrapper 
 
    
-
-def print_vec(t,v):
-    print(t)
-    for e in v:
-       print(e)
-
-
-def print_vvec(t,v):
-    print(t)
-    for vv in v:
-      for e in vv:
-        print(e)
-      print("===")
-
-def print_layer(l):
-    print(dir(l))
-    #print(l.type)
-    #print_layer_param(l)
-    print("blobs:%d\n"%(len(l.blobs)))
-    #print_vec(l.__dict__)
-    sys.exit(0)
-
-def print_blob(l):
-    #print(dir(l))
-    #print_layer_param(l)
-    #print("blobs:%d\n"%(len(l.blobs)))
-    #print_vec(l.__dict__)
-    print("%s,%s,%s,%s,%s"%(l.channels,l.count,l.height,l.num,l.width))
-    for d in l.shape:
-        print(d)
-    print(len(l.data))
-    print(len(l.diff))
-
-def print_net(n):
-    
-    #print_vec("backward()backward()",n.backward())
-    #print_vec("_batch",n._batch())
-    #print_vec("_blob_loss_weights",n._blob_loss_weights)
-   
-    print_vec("_blob_names:",n._blob_names)
-    for b in n._blobs:
-        print_blob(b)
-    i=0
-    for l in n.layers:
-      print_vec("_bottom_ids():",n._bottom_ids(i))
-      print_vec("_top_ids():",n._top_ids(i))
-      i=i+1
-#    print(dir(n._forward))
-#    print(dir(n._inputs))
-#    print_vec("_layer_names:",n._layer_names)
-#    print(dir(n._outputs))
-
-    print_vec("outputs:",n.outputs)
-    print_vec("inputs:",n.inputs)
-#    for l in n.layers:
-#       print_layer(l)
-#    print(dir(n.params))
-#    print(dir(n.params.__class__))
-#    for k in n.params.keys():
-#         print (k)
-#    print(dir(n.blob_loss_weights))
-#    print(dir(n.blobs))
-    print_vec("bottom_names:",n.bottom_names)
-    print_vec("top_names:",n.top_names)
-#clear_param_diffs
-#copy_from
-#backward
-#forward
-#forward_all
-#forward_backward_all
-#load_hdf5
-#reshape
-#save
-#save_hdf5
-#set_input_arrays
-#_set_input_arrays
-#share_with
-
 
 
 if len(sys.argv)<3:
@@ -100,11 +22,7 @@ print(weights)
 net = caffe.Net(model, caffe.TEST)
 net.copy_from(weights)
 
-#print(len(net._blob_names))
-#print(len(net.top_names))
-#print(len(net.bottom_names))
-#print_net(net)
-#sys.exit(0)
+net_=net_wrapper(net)
 
 with open("gen/net_blobs.cpp","w") as f:
     f.write("""
@@ -113,10 +31,10 @@ void sc_net::create_blobs()
 {
 blobs.resize(%d);
 """
-    %(len(net._blobs)))
+    %(len(net_._blobs)))
     i=0
-    for b in net._blobs:
-      f.write("/*%s*/\n"%(net._blob_names[i]))
+    for b in net_._blobs:
+      f.write("/*%s*/\n"%(net_._blob_names[i]))
       f.write("blobs[%d]=new caffe::Blob<float>(%d,%d,%d,%d);\n"%(i,b.num,b.channels,b.height,b.width))
       i=i+1
 
@@ -125,11 +43,11 @@ blobs.resize(%d);
     i=0
     for l in net.layers:
        name=net._layer_names[i]
-       for id in net._top_ids(i):
-         f.write("/*%s*/\n"%(net._blob_names[id]))
+       for id in net_._top_ids(i):
+         f.write("/*%s*/\n"%(net_._blob_names[id]))
          f.write("%s.append_top(blobs[%d]);\n"%(name,id))
-       for id in net._bottom_ids(i):
-         f.write("/*%s*/\n"%(net._blob_names[id]))
+       for id in net_._bottom_ids(i):
+         f.write("/*%s*/\n"%(net_._blob_names[id]))
          f.write("%s.append_bottom(blobs[%d]);\n"%(name,id))
 
        i=i+1
---
+++ net_wrapper.py
@@ -0,0 +1,71 @@
+class dup_blob_rec:
+  def __init__(self,_origin_blob_idx,_clone_blob_idx):
+      self.origin_blob_idx=_origin_blob_idx
+      self.clone_blob_idx=_clone_blob_idx
+
+class net_wrapper:
+  
+  def __init__(self,_net):
+      self.net=_net
+      self.records=[]
+      self.top_ids=[]
+      self.bottom_ids=[]
+      self._blobs=[]
+      self._blob_names=[]
+      self.build()
+
+  def add_blob(self,blob_idx):
+      new_idx = len(self._blobs)
+      self._blob_names.append('%s_clone'%(self.net._blob_names[blob_idx]))
+      self._blobs.append(self.net._blobs[blob_idx])
+      r=dup_blob_rec(blob_idx,new_idx)
+      self.records.append(r)
+      return new_idx
+
+  def convert_blob(self,blob_idx):
+       for r in self.records:
+           if r.origin_blob_idx==blob_idx :
+#                print("convert %d to %d"%(blob_idx, r.clone_blob_idx))
+                return r.clone_blob_idx
+       return blob_idx 
+
+  def count(self):
+      return len(self._blobs)
+
+  def _top_ids(self,i):
+      return self.top_ids[i]
+
+  def _bottom_ids(self,i):
+      return self.bottom_ids[i]
+
+  def convert(self,s):
+      r=[]
+      for b in s:
+          n=self.convert_blob(b)
+          r.append(b)
+      return r
+
+  def build(self):
+     for n in self.net._blob_names:
+        self._blob_names.append(n)
+ 
+     for b in self.net._blobs:
+        self._blobs.append(b)
+
+     i=0
+     for l in self.net.layers:
+        top=self.net._top_ids(i)
+        bottom=self.net._bottom_ids(i)
+#        if len(top)>0 and len(bottom)>0:
+#           print("%d,%d"%(top[0],bottom[0]))
+        if len(top)==1 and len(bottom)==1 and top[0]==bottom[0]:
+           idx=top[0]
+           new_idx=self.add_blob(idx)
+           self.top_ids.append([new_idx]) 
+           self.bottom_ids.append([idx]) 
+        else:
+           _top=self.convert(top)
+           _bottom=self.convert(bottom)
+           self.top_ids.append(_top)
+           self.bottom_ids.append(_bottom)
+        i=i+1
